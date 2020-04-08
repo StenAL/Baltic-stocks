a27b1e4bd67e59acd1fbd98c45e518b7e61b3301
Sten Laane <laanesten@gmail.com>
2020-04-08 11:30:30 +0100

    Add CI/CD workflow for frontend
    Signed-off-by: Sten Laane <laanesten@gmail.com>

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -93,7 +93,7 @@ i18n
     .init({
         resources,
         fallbackLng: 'en',
-        debug: true,
+        debug: false,
 
         interpolation: {
             escapeValue: false, // not needed for react as it escapes by default
