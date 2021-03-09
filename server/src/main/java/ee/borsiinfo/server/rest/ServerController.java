499af27c26d04e170ac0ba57713d89249a4f140b
Sten Laane <sten@laane.xyz>
2021-03-09 16:17:59 +0200

    Wrap /stocks requests in a try-catch.
    This prevents the whole application from crashing when a request fails

--- server/src/main/java/ee/borsiinfo/server/rest/ServerController.java
+++ server/src/main/java/ee/borsiinfo/server/rest/ServerController.java
@@ -7,6 +7,7 @@ import ee.borsiinfo.server.repository.IndexRepository;
 import ee.borsiinfo.server.repository.StockRepository;
 import ee.borsiinfo.server.service.importing.DataImportingJob;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 
+@Log4j2
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -30,10 +32,16 @@ public class ServerController {
     @CrossOrigin("*")
     public DataResponse getAllData() {
         LocalDateTime fetchAfter = LocalDateTime.now().minusDays(DataImportingJob.FETCH_FREQUENCY_DAYS);
-        List<Stock> stocks = stockRepository.findAllByTimeFetchedAfter(fetchAfter);
-        Index index = indexRepository.findAllByTimeFetchedAfter(fetchAfter).get(0);
-        LocalDateTime timeFetched = stocks.get(0).getTimeFetched().truncatedTo(ChronoUnit.HOURS);
-        return new DataResponse(stocks, timeFetched, index);
+        try {
+            List<Stock> stocks = stockRepository.findAllByTimeFetchedAfter(fetchAfter);
+            Index index = indexRepository.findAllByTimeFetchedAfter(fetchAfter).get(0);
+            LocalDateTime timeFetched = stocks.get(0).getTimeFetched().truncatedTo(ChronoUnit.HOURS);
+            return new DataResponse(stocks, timeFetched, index);
+        } catch (IndexOutOfBoundsException e) {
+            log.error("Fail to fetch stocks: " + e.getMessage());
+            log.error(e);
+            return new DataResponse(null, LocalDateTime.now(), null);
+        }
     }
 
     @PostMapping("/importAll")
