43872d62dfa1c1793883a5a9bf7eb4da80551a8a
Sten Laane <laanesten@gmail.com>
2022-02-21 09:11:24 +0000

    Server: Do not send null-values from backend
    If properties don't exist, we can just omit them. This will make
    frontend types simpler.

--- server/src/main/java/ee/borsiinfo/server/configuration/ObjectMapperConfiguration.java
+++ server/src/main/java/ee/borsiinfo/server/configuration/ObjectMapperConfiguration.java
@@ -1,9 +1,9 @@
 package ee.borsiinfo.server.configuration;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,8 +12,10 @@ import org.springframework.context.annotation.Configuration;
 public class ObjectMapperConfiguration {
 
     @Bean
-    public Jackson2ObjectMapperBuilderCustomizer objectMapperBuilderCustomizer() {
-        return jacksonObjectMapperBuilder -> jacksonObjectMapperBuilder.modulesToInstall(new Jdk8Module(), new JavaTimeModule())
-            .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper()
+            .registerModule(new JavaTimeModule())
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .setSerializationInclusion(JsonInclude.Include.NON_NULL);
     }
 }
