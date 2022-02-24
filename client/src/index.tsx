928d880850ca666f2023dde414d84f7feffa50f5
Sten Laane <laanesten@gmail.com>
2022-02-24 08:48:15 +0000

    Client: Make tsconfig stricter

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import { App } from "./App";
 import "./i18n";
 
-ReactDOM.render(
+render(
     <StrictMode>
         <App />
     </StrictMode>,
