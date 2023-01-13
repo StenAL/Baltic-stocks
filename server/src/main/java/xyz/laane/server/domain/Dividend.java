500738773c9ec95f3143c7874b1eec8e4193f211
Sten Laane <21343173+StenAL@users.noreply.github.com>
2023-01-13 20:44:32 +0000

    Server: Upgrade to Spring Boot 3
    Since this was already running Java 17, the upgrade was easy, just
    renaming 'javax' imports to 'jakarta'

--- server/src/main/java/xyz/laane/server/domain/Dividend.java
+++ server/src/main/java/xyz/laane/server/domain/Dividend.java
@@ -3,7 +3,7 @@ package xyz.laane.server.domain;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.time.LocalDate;
 
 @Data
