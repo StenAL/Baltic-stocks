037b2db97b4bb36182cc186978d517e2cf45847f
Sten Laane <21343173+StenAL@users.noreply.github.com>
2024-07-01 14:17:24 +0300

    Everywhere: Update dependencies
    Nothing too noteworthy. MariaDB removed mysql compatibility symlinks in
    v11 so now Docker compose files need to use mariadbd instead of mysqld
    as the entrypoint.

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -3,10 +3,10 @@ version = "1.0.0"
 
 plugins {
     java
-    id("org.springframework.boot") version "3.1.4"
-    id("io.spring.dependency-management") version "1.1.3"
-    id("com.gorylenko.gradle-git-properties") version "2.4.1"  // Used by Sentry to match errors to specific git commit
-    id("io.freefair.lombok") version "8.4" // Lombok 1.18.30
+    id("org.springframework.boot") version "3.3.1"
+    id("io.spring.dependency-management") version "1.1.5"
+    id("com.gorylenko.gradle-git-properties") version "2.4.2"  // Used by Sentry to match errors to specific git commit
+    id("io.freefair.lombok") version "8.6" // Lombok 1.18.30
 }
 
 java {
@@ -21,11 +21,11 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
-    implementation("org.mariadb.jdbc:mariadb-java-client:3.2.0")
-    implementation("org.flywaydb:flyway-core:9.22.3")
-    implementation("org.flywaydb:flyway-mysql:9.22.3")
+    implementation("org.mariadb.jdbc:mariadb-java-client:3.4.0")
+    implementation("org.flywaydb:flyway-core:10.15.0")
+    implementation("org.flywaydb:flyway-mysql:10.15.0")
 
-    implementation("org.jsoup:jsoup:1.16.2")
+    implementation("org.jsoup:jsoup:1.17.2")
 
     implementation("io.sentry:sentry-spring-boot-starter-jakarta:6.32.0")
     implementation("io.sentry:sentry-logback:6.32.0")
