e1db54e83ca9f7b35e813056c9cc99115e1e8e19
Sten Laane <21343173+StenAL@users.noreply.github.com>
2025-02-22 11:46:12 +0200

    Server: Follow redirects in stock data API
    The default URL now returns a 302 and redirects to the actual API.

--- server/src/main/java/xyz/laane/server/service/importing/StockDataImportingService.java
+++ server/src/main/java/xyz/laane/server/service/importing/StockDataImportingService.java
@@ -32,6 +32,7 @@ public class StockDataImportingService {
         Document doc;
         try {
             doc = Jsoup.connect(url).userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36")
+                .followRedirects(true)
                 .timeout(120 * 1000).get();
         } catch (IOException e) {
             log.error("Failed to connect to data API for ISIN {}", importingRequest.isin(), e);
