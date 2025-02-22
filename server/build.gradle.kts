135d1c5ad3a4efb694314d024325f2b892791c63
Sten Laane <21343173+StenAL@users.noreply.github.com>
2025-02-22 16:19:07 +0200

    Server: Get dependency versions from Spring Boot

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -3,7 +3,7 @@ version = "1.0.0"
 
 plugins {
     java
-    id("org.springframework.boot") version "3.3.1"
+    id("org.springframework.boot") version "3.4.3"
     id("io.spring.dependency-management") version "1.1.5"
     id("io.freefair.lombok") version "8.6" // Lombok 1.18.30
 }
@@ -20,9 +20,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
-    implementation("org.mariadb.jdbc:mariadb-java-client:3.4.0")
-    implementation("org.flywaydb:flyway-core:10.15.0")
-    implementation("org.flywaydb:flyway-mysql:10.15.0")
+    implementation("org.mariadb.jdbc:mariadb-java-client")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-mysql")
 
     implementation("org.jsoup:jsoup:1.17.2")
 }
