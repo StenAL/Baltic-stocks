02a6f213183554046be09da2ef3e7c178c9b5ff5
Sten Laane <21343173+StenAL@users.noreply.github.com>
2025-02-24 13:35:26 +0200

    Client: Import assets directly instead of using import.meta
    Vite can just import assets without needing to use import.meta to figure
    out the path. Also, the logo displayed on the site is now an SVG instead
    of a png.

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,12 +1,13 @@
 import { FunctionComponent } from "react";
 import "../style/Footer.css";
+import footer from "../assets/footer.png";
 import { useTranslation } from "react-i18next";
 
 export const Footer: FunctionComponent = () => {
     const { t } = useTranslation();
     return (
         <footer>
-            <img src={`${import.meta.env.BASE_URL}/footer.png`} className="footer" alt="Skyline view of Tallinn" />
+            <img src={footer} className="footer" alt="Skyline view of Tallinn" />
             <p>
                 {t("this app is")} <a href="https://github.com/StenAL/baltic-stocks">{t("open-source")}</a>
             </p>
