b2e4e2478e39794d656cb8d6831d60f9eb75e484
Sten Laane <21343173+StenAL@users.noreply.github.com>
2025-02-24 16:14:04 +0200

    Server: Use Java records for DTOs
    No need for Lombok for these anymore.

--- server/src/main/java/xyz/laane/server/dto/DataResponse.java
+++ server/src/main/java/xyz/laane/server/dto/DataResponse.java
@@ -3,17 +3,13 @@ package xyz.laane.server.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import xyz.laane.server.domain.Index;
 import xyz.laane.server.domain.Stock;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Data
-@AllArgsConstructor
-public class DataResponse {
-    private List<Stock> stocks;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm dd.MM.yyyy")
-    private LocalDateTime timeFetched;
-    private Index index;
+public record DataResponse(
+        List<Stock> stocks,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm dd.MM.yyyy")
+        LocalDateTime timeFetched,
+        Index index) {
 }
