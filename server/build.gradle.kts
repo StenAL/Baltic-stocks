e7060a354f1fdbe14a6b07eab9f4a5c74e00cacc
Sten Laane <21343173+StenAL@users.noreply.github.com>
2025-02-24 16:12:17 +0200

    Server: Add spring-boot-starter-cache dependency
    This makes IntelliJ no longer complain about not having a CacheManager
    bean.

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -18,6 +18,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-cache")
 
     implementation("org.mariadb.jdbc:mariadb-java-client")
     implementation("org.flywaydb:flyway-core")
